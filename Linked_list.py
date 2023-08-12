fd645c31c9f5b6da33d29fdadf9c0fe8af45a817
Ranrovel <ranrovel1611@protonmail.com>
2023-08-12 23:29:56 +0530

    Linked link
    Creation, insertion, printing

---
+++ Linked_list.py
@@ -0,0 +1,51 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class link_list:
+    def __init__(self):
+        self.head = None
+
+    def insert(self, new_node):
+        # If head is none
+        if self.head is None:
+            self.head = new_node
+
+        # Insert node untill node.next is not none
+        else:
+            last_node = self.head
+            while last_node.next is not None:
+                last_node = last_node.next
+            last_node.next = new_node
+
+    def print_list(self):
+        # If No node in linked list
+        if self.head is None:
+            print("Linked List is empty")
+            return
+
+        # Print node data untill its not nonw
+        curr_node = self.head
+        while curr_node is not None:
+            print(curr_node.data)
+            curr_node = curr_node.next
+
+
+# Node => Data,next
+# firstNode.data => john, firstNode.next => None
+first_node = Node("Gaurav")
+sec_node = Node("Tilak")
+third_node = Node("Harsh")
+
+# Create Linked List
+Linked_list = link_list()
+
+# Insert Nodes
+Linked_list.insert(first_node)
+Linked_list.insert(sec_node)
+Linked_list.insert(third_node)
+
+# Print List
+Linked_list.print_list()
