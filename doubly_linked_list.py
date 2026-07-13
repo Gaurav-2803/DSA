6e15661dd8f8ba842d58b08d193360e82573fe12
Gaurav-2803 <ranrovel1611@protonmail.com>
2026-07-13 08:23:16 +0530

    DLL Insertion and __str__  methods added

--- doubly_linked_list.py
+++ doubly_linked_list.py
@@ -11,11 +11,22 @@ class DoublyLinkedList:
     def __init__(self):
         self.head = None
         self.tail = None
-        self.length = 0
+        self._length = 0
 
     def __len__(self) -> int:
         """Return Length of DLL"""
-        return self.length
+        return self._length
+
+    def __str__(self) -> int:
+        """Return DLL in String"""
+        values = []
+        current = self.head
+        while current:
+            values.append(str(current.data))
+            current = current.next
+
+        values.append("None")
+        return " -> ".join(values)
 
     def traverse(self) -> None:
         current = self.head
@@ -30,17 +41,63 @@ class DoublyLinkedList:
 
         if self.head is None:
             self.head = self.tail = new_node
-            self.length += 1
+            self._length += 1
             return
 
         new_node.back = self.tail
         self.tail.next = new_node
         self.tail = new_node
-        self.length += 1
+        self._length += 1
+
+    def insert(self, element, idx) -> None:
+        if idx < 0 or idx > self._length:
+            return
+
+        new_node = Node(element)
+
+        # Empty list
+        if self._length == 0:
+            self.head = self.tail = new_node
+            self._length += 1
+            return
+
+        # Insert at head
+        if idx == 0:
+            new_node.next = self.head
+            self.head.back = new_node
+            self.head = new_node
+            self._length += 1
+            return
+
+        # Insert at tail
+        if idx == self._length:
+            new_node.back = self.tail
+            self.tail.next = new_node
+            self.tail = new_node
+            self._length += 1
+            return
+
+        # Traverse to node at index `idx`
+        if idx <= self._length // 2:
+            current = self.head
+            for _ in range(idx):
+                current = current.next
+        else:
+            current = self.tail
+            for _ in range(self._length - idx):
+                current = current.back
+
+        # Insert BEFORE current
+        new_node.back = current.back
+        new_node.next = current
+        current.back.next = new_node
+        current.back = new_node
+
+        self._length += 1
 
     def delete(self, index: int) -> None:
         """Delete element by index"""
-        if self.head is None or index < 0 or index >= self.length:
+        if self.head is None or index < 0 or index >= self._length:
             return
 
         if index == 0:
@@ -50,12 +107,17 @@ class DoublyLinkedList:
             else:
                 self.head.back = None
 
-            self.length -= 1
+            self._length -= 1
             return
 
-        current = self.head
-        for _ in range(index):
-            current = current.next
+        if index <= self._length // 2:
+            current = self.head
+            for _ in range(index):
+                current = current.next
+        else:
+            current = self.tail
+            for _ in range(self._length - index - 1):
+                current = current.back
 
         if current != self.tail:
             current.next.back = current.back
@@ -63,8 +125,8 @@ class DoublyLinkedList:
             self.tail = current.back
 
         current.back.next = current.next
-        current.next = current.prev = None
-        self.length -= 1
+        current.next = current.back = None
+        self._length -= 1
 
     def convert_arr_to_dll(self, arr):
         """Convert array into DLL"""
@@ -78,9 +140,11 @@ dll.append("1")
 dll.append("2")
 dll.append("3")
 dll.append("4")
-dll.traverse()
+print(str(dll))
 dll.delete(3)
-dll.traverse()
+print(str(dll))
+dll.insert(3, 3)
+print(str(dll))
 # dll.append(1)
 # dll.append(1.0)
 # dll.convert_aar_to_dll([21, 324, 324, "svdvd", ["d", 3]])
